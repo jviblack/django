611a2d8baecb368814b3f34ecdbe7005f9ec2831
jviblack <javie.couto.dominguez@gmail.com>
2023-09-23 04:00:29 +0200

    commit

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+import datetime
 
 def saludo(request):
 
-    return HttpResponse ("Hola a todos")
+    docu = """<html><body><h1>
+
+    Hola a todos
+
+    </h1></body></html>"""
+
+    return HttpResponse (docu)
+
+def fecha(request):
+
+    fecha_actual=datetime.datetime.now()
+
+     docu = """<html><body><h1>
+
+    Fecha y hora actuales %s
+
+    </h1></body></html>""" % fecha_actual
+
+    return HttpResponse (docu)
