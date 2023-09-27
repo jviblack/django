d9fb8034dab9d74187b178ea193c2fb34a9082ba
jviblack <javie.couto.dominguez@gmail.com>
2023-09-27 06:13:19 +0200

    commit

--- Proyecto1/TiendaOnline/GestionPedidos/models.py
+++ Proyecto1/TiendaOnline/GestionPedidos/models.py
@@ -5,28 +5,25 @@ from django.db import models
 class Clientes(models.Model):
 
     nombre = models.CharField(max_length=30)
-
     direccion = models.CharField(max_length=50)
-
     email = models.EmailField()
-
     tfno = models.CharField(max_length=12)
 
 
 class Articulos(models.Model):
 
     nombre = models.CharField(max_length=30)
-
     seccion = models.CharField(max_length=20)
-
     precio = models.IntegerField()
+
+    def __str__(self):
+
+        return 'El nombre es %s, la sección es %s y el precio es %s' % (self.nombre, self.seccion, self.precio)
 
 
 class Pedidos(models.Model):
 
     numero = models.IntegerField()
-
     fecha = models.DateField()
-
     entregado = models.BooleanField()
 
