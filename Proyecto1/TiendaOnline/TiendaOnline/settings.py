dffa6d616accda3fd5bf4645fc48eb5c48871de0
jviblack <javie.couto.dominguez@gmail.com>
2023-09-27 05:31:13 +0200

    commit

--- Proyecto1/TiendaOnline/TiendaOnline/settings.py
+++ Proyecto1/TiendaOnline/TiendaOnline/settings.py
@@ -75,12 +75,11 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'default': { 
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
