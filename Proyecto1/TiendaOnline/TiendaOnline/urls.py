75f6a0cb8c41cb3983bd5be79f6ecbc58b50327a
jviblack <javie.couto.dominguez@gmail.com>
2023-09-28 06:23:31 +0200

    commit

--- Proyecto1/TiendaOnline/TiendaOnline/urls.py
+++ Proyecto1/TiendaOnline/TiendaOnline/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from GestionPedidos import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('busqueda_productos/', views.busqueda_productos),
+    path('buscar/', views.buscar)
 ]
