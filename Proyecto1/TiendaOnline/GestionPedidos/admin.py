eef156410ff8ffdb574ec5c8fb6ab77763f7a1fb
jviblack <javie.couto.dominguez@gmail.com>
2023-09-28 05:32:27 +0200

    commit

--- Proyecto1/TiendaOnline/GestionPedidos/admin.py
+++ Proyecto1/TiendaOnline/GestionPedidos/admin.py
@@ -3,6 +3,13 @@ from GestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
 
-admin.site.register(Clientes)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "tfno", "direccion")
+    search_fields = ("nombre", "tfno")
+
+
+cla
+
+admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
--- Proyecto1/TiendaOnline/GestionPedidos/models.py
+++ Proyecto1/TiendaOnline/GestionPedidos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Clientes(models.Model):
 
     nombre = models.CharField(max_length=30)
-    direccion = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50, verbose_name='La dirección')
     email = models.EmailField(blank=True, null=True)
     tfno = models.CharField(max_length=12)
 
