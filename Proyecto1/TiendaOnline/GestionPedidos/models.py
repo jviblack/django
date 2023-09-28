d8bb668f4874767d64a85873d345c6b630522bf6
jviblack <javie.couto.dominguez@gmail.com>
2023-09-28 05:14:46 +0200

    commit

--- Proyecto1/TiendaOnline/GestionPedidos/models.py
+++ Proyecto1/TiendaOnline/GestionPedidos/models.py
@@ -6,9 +6,12 @@ class Clientes(models.Model):
 
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField(blank=True, null=True)
     tfno = models.CharField(max_length=12)
 
+    def __str__(self):
+
+        return 'El nombre del cliente es %s' % (self.nombre)
 
 class Articulos(models.Model):
 
