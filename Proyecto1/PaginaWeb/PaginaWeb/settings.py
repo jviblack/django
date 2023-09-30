a20a20eb1b4defaa75ed7b39b4da3267e67d95fa
jviblack <javie.couto.dominguez@gmail.com>
2023-09-30 14:41:31 +0200

    commit

--- Proyecto1/PaginaWeb/PaginaWeb/settings.py
+++ Proyecto1/PaginaWeb/PaginaWeb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)*bj#rinx6v&w86vec#%a5tb11u2wxd(_+_x!#pw-(ps(9uw^*'
+SECRET_KEY = 'django-insecure-)*bj#rinx6v&w86vec#%a5tb11u2wxd(_+_x!#pw-(ps(9uw^*-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
