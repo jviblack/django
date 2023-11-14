6a878af518ad7304ac421b257b001ec18b78534d
jviblack <javie.couto.dominguez@gmail.com>
2023-11-14 05:33:20 +0100

    commit

--- Proyecto1/PaginaWeb/PaginaWeb/urls.py
+++ Proyecto1/PaginaWeb/PaginaWeb/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     #path('tienda/', views.tienda, name="Tienda"),
     #path('blog/', views.blog, name="Blog"),
     #path('contacto/', views.contacto, name="Contacto"),
-    path('WebApp/', include('WebApp.urls')),
+    path('', include('WebApp.urls')),
 ]
