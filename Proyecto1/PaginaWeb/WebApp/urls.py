f4a3b66e069836b65a78e4b0b8a3405d3076565b
jviblack <javie.couto.dominguez@gmail.com>
2023-11-16 06:35:16 +0100

    commit

--- Proyecto1/PaginaWeb/WebApp/urls.py
+++ Proyecto1/PaginaWeb/WebApp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from WebApp import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #path('admin/', admin.site.urls), está en el urls.py de la paginaweb
@@ -9,4 +11,7 @@ urlpatterns = [
     path('tienda/', views.tienda, name="Tienda"),
     path('blog/', views.blog, name="Blog"),
     path('contacto/', views.contacto, name="Contacto"),
+    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
